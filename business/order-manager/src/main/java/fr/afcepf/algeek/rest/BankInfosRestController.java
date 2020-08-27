050fe811460c5b1d37514ad2583462ffd74e2093
Benjamin Bonche <benjamin.bonche@gmail.com>
2020-08-27 16:23:22 +0200

    livraison partielle

--- business/order-manager/src/main/java/fr/afcepf/algeek/rest/BankInfosRestController.java
+++ business/order-manager/src/main/java/fr/afcepf/algeek/rest/BankInfosRestController.java
@@ -3,10 +3,15 @@ package fr.afcepf.algeek.rest;
 import fr.afcepf.algeek.dto.Commande;
 import fr.afcepf.algeek.dto.InfosBancaires;
 import fr.afcepf.algeek.dto.LigneCommande;
+import fr.afcepf.algeek.service.BankInfosService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BankInfosRestController {
+
+    @Autowired
+    private BankInfosService bankInfosService;
 
 
 }
